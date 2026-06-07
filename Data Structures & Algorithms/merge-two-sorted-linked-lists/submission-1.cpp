ade569ff69a98f20580bdfd819e5287b5fd835ad
PrachiGuptaCSE <prachigupta.cse@gmail.com>
2026-06-07 21:42:44 +0530

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode * dummy=new ListNode();
+        ListNode *curr=dummy;
+        while(l1 != NULL && l2 !=NULL){
+            if(l1->val<l2->val){
+                curr->next=l1;
+                l1=l1->next;
+            }
+            else{
+                curr->next=l2;
+                l2=l2->next;
+            }
+            curr=curr->next;
+        }
+        if(l1!=NULL){
+            curr->next=l1;
+        }
+        else{
+            curr->next=l2;
+        }
+    return dummy->next;   
+    }
+};
